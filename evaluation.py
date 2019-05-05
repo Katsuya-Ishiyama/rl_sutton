d390f30b3d1ec0ca80ea3c9d10c8e337704faed1
Katsuya Ishiyama <ishiyama.katsuya@gmail.com>
2019-05-05 11:24:06 +0900

    changed to calculate average over iterations.

--- evaluation.py
+++ evaluation.py
@@ -59,22 +59,21 @@ def calculate_average_suitable_action_rate(output_dir):
     logger.debug('Exploratory Rate: {}'.format(exploratory_rate))
 
     play_count_list = []
-    suitable_action_rate_list = []
+    suitable_action_flag_list = []
     for file in _output_dir.iterdir():
         src_data = pd.read_csv(file)
         if not play_count_list:
             play_count_list.extend(src_data.play_count.tolist())
             logger.debug('Extracted Play Counts: {}'.format(play_count_list))
         is_suitable_action = src_data.action == src_data.most_suitable_action
-        suitable_action_count = is_suitable_action.cumsum()
-        suitable_action_rate = suitable_action_count.div(src_data.play_count)
-        suitable_action_rate_list.append(suitable_action_rate.tolist())
+        suitable_action_flag = 1 * is_suitable_action
+        suitable_action_flag_list.append(suitable_action_flag.tolist())
 
     average_suitable_action_rate_by_play_count = pd.DataFrame(
         data={
             'play_count': play_count_list,
             'exploratory_rate': exploratory_rate,
-            'average_suitable_action_rate': calculate_average_by_play_count(suitable_action_rate_list)
+            'average_suitable_action_rate': calculate_average_by_play_count(suitable_action_flag_list)
         }
     )
     logger.debug('calculate_average_suitable_action_rate: {}'.format(average_suitable_action_rate_by_play_count.head()))
